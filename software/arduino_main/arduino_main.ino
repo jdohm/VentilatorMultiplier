e5f30d3254d1945c9c7eef2eaed3e317d7ff237f
jdohm <jannismax@gmail.com>
2020-03-22 17:14:59 +0100

    Added a few thoughts to the Software part
    Version 0.0.4

--- software/arduino_main/arduino_main.ino
+++ software/arduino_main/arduino_main.ino
@@ -1,4 +1,4 @@
-// uncomment the sensur you are going to use:
+/*--- uncomment the sensur you are going to use: ---*/
 #define SFM3000 true
 #define HAFUHH0200L2AXT true
 #define HAFUHH0200L3AXT true
@@ -7,20 +7,35 @@
 #define HAFUHH0200L6AXT true
 #define HAFUHH0200L7AXT true
 #define TSI840523 true
+/*--- Number of flow sensors ---*/
+#define CntSensor 1
+#define CntSensor 2
+#define CntSensor 3
+#define CntSensor 4
+/*--- Set allowed diefference between sensors (in percent) ---*/
+#define AllowedPercentage 10 //not a suggested value, just a random value!
 
 #include "Wire.h"
 
 void setup() {
   // put your setup code here, to run once:
-#if !TSI840523
+#ifndef TSI840523
   Wire.begin();
-#endif //!TSI840523  
+#endif //TSI840523  
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  
+
+  //CheckDifference if more than one Sensor
+  if(CntSensor > 1){
+    if(!CheckDifference(AllowedPercentage))
+      StartAlarm();
+    
+  }
 }
 
 #if TSI840523
@@ -31,5 +46,10 @@ bool CheckPlausibility(){
 bool CheckDifference(uint8_t percent){
 
    
+}
+
+void StartAlarm(){
+
+  
 }
 #endif //TSI840523
